701a94432f30e164e35e480d1e837c059753de39
Jared <jjohns3@tulane.edu>
2022-10-03 17:54:07 -0500

    Initial file import and repo creations. Set up the db schema as well.

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS workplace_db;
+CREATE DATABASE workplace_db;
+
+USE workplace_db;
+
+CREATE TABLE department(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+CREATE TABLE role(
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY role(department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id(INT),
+    FOREIGN KEY employee(role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL
+);
